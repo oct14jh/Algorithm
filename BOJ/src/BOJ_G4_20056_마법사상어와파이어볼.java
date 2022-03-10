368cf0884e6712b92eae1927d104d304555d5b90
oct14jh <khyun7621@naver.com>
2022-03-10 23:03:55 +0900

    BOJ_G4_20056_마법사상어와파이어볼::시뮬레이션

---
+++ BOJ/src/BOJ_G4_20056_마법사상어와파이어볼.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_20056_마법사상어와파이어볼 {
+	
+	static class FireBall {
+		int r, c, m, s, d;
+
+		public FireBall(int r, int c, int m, int s, int d) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.m = m;
+			this.s = s;
+			this.d = d;
+		}
+	}
+	
+	private static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+	private static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
+
+/**
+ * 														
+ */
