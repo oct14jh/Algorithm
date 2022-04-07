44fc2ef97664f6e15223023d2a90a9b9aa6875d7
oct14jh <khyun7621@naver.com>
2022-04-07 23:18:26 +0900

    BOJ_S2_2644_촌수계산::쉬운데왜이걸...

--- BOJ/src/BOJ_S2_2644_촌수계산.java
+++ BOJ/src/BOJ_S2_2644_촌수계산.java
@@ -1,46 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class BOJ_S2_2644_촌수계산 {
-	private static int N, A, B, M, result;
-	private static int[][] relations;
-	private static BufferedReader br;
-	private static StringTokenizer st;
-	public static void main(String[] args) throws IOException {
-		
-		processInput();
-		
-		processOutput();
-		System.out.println(result);
-		
-	}
-	
-	private static void processInput() throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
 
-		st = new StringTokenizer(br.readLine(), " ");
+	private static int N, A, B, M, result = -1;
+	private static boolean[] visited;
+	private static int[][] relation;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		N = Integer.parseInt(st.nextToken());
+		relation = new int[N+1][N+1];
+		visited = new boolean[N+1];
 		
 		st = new StringTokenizer(br.readLine(), " ");
 		A = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
 		
-		st = new StringTokenizer(br.readLine(), " ");
-		M = Integer.parseInt(st.nextToken());
-		
-		relations = new int[M][2];
-		
+		M = Integer.parseInt(br.readLine());
 		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			relations[i][0] = x; // 자식
-			relations[i][1] = y; // 부모
+			relation[x][y] = 1;
+			relation[y][x] = 1;
 		}
-	}
-
-	private static void processOutput() {
 		
+		process(A, 0);
+		System.out.println(result);
+	}
+	
+	private static void process(int person, int depth) {
+		visited[person] = true;
+		
+		if(person == B) {
+			result = depth;
+			return;
+		}
+		
+		for(int index=1; index < N+1; index++) {
+			if(!visited[index] && relation[person][index] == 1) {
+				visited[index] = true;
+				process(index, depth+1);
+				visited[index] = false;
+			}
+		}
 	}
 }
