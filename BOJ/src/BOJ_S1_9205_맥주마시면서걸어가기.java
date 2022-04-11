2b86793ffa6423ac39737717419ae89f81b26cd1
oct14jh <khyun7621@naver.com>
2022-04-12 00:50:13 +0900

    BOJ_S1_9205_맥주마시면서걸어가기:편의점으로체크하기에, 페스티벌위치도 편의점으로 체크해야함

--- BOJ/src/BOJ_S1_9205_맥주마시면서걸어가기.java
+++ BOJ/src/BOJ_S1_9205_맥주마시면서걸어가기.java
@@ -1,7 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class BOJ_S1_9205_맥주마시면서걸어가기 {
-	public static void main(String[] args) {
+	private static class Pos {
+		int r, c;
+		public Pos(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+	}
+	private static int t, n;
+	private static Pos player, festival;
+	private static Pos[] CU;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		t = Integer.parseInt(br.readLine());
+		
+		for(int testCase = 0; testCase < t; testCase++) {
+			n = Integer.parseInt(br.readLine());
+			CU = new Pos[n+1];
+			
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			player = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			
+			for(int i = 0; i <= n; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				CU[i] = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+
+//			st = new StringTokenizer(br.readLine(), " ");
+//			festival = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			
+			String result = process();
+			System.out.println(result);
+		}
+	}
+	
+	private static String process() {
+		Queue<Pos> q = new LinkedList<>();
+		boolean[] visited = new boolean[n+1];
+		Pos current = player;
+		q.offer(current);
+		while(!q.isEmpty()) {
+			current = q.poll();
+			if((current.r == CU[n].r) && (current.c == CU[n].c))
+				return "happy";
+			
+			for(int i = 0; i <= n; i++) {
+				if(!visited[i] && !isOverDistance(current, CU[i])) {
+					q.add(CU[i]);
+					visited[i] = true;
+				}
+			}
+		}
+		return "sad";
+	}
+	
+	private static boolean isOverDistance(Pos a, Pos b) {
+		if(Math.abs(a.r-b.r) + Math.abs(a.c-b.c) <= 1000)
+			return false;
+		return true;
 	}
 }
 
@@ -24,7 +88,4 @@ for(int t = 0; t < tc; t++) {
 	상근이집 좌표
 	락페스티벌좌표
 }
-
-상근이
-
 */
